fa65731b69c57bddbd58e6a8673448d53bcce3cc
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-24 14:23:42 -0400

    add gitignore and start to project

---
+++ src/main/java/com/detroitlabs/mysqlauth/MysqlauthApplication.java
@@ -0,0 +1,13 @@
+package com.detroitlabs.mysqlauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/detroitlabs/mysqlauth/MysqlauthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.detroitlabs.mysqlauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
